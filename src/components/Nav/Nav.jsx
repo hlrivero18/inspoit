b1c01cfab624c56ae5a7f7df654d0db15370e43b
Héctor Luis Rivero <hectorluisrivero14@gmail.com>
2024-08-14 15:13:16 -0300

    mejorando el nav

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,24 +1,25 @@
 import style from "./Nav.module.css"
 import { MdSearch } from "react-icons/md";
 import { Lato } from "next/font/google";
+import logo from "@/images/logoI.png"
 
 const lato = Lato({ subsets: ["latin"], weight: ["700"] })
 
 export default function Navbar() {
     return (
-        <nav className={`${lato.className} ${style.nav}`}>
-            <section>
-                logo imaginario
+        <nav className={` ${style.nav}`}>
+            <section className={`${style.nav__logo}`}>
+                <img src={logo.src} alt="" />
             </section>
             <ul>
-                <li>Red</li>
-                <li>Contacto</li>
-                <li>Nosotros</li>
+                <li>RED</li>
+                <li>CONTACTO</li>
+                <li>NOSOTROS</li>
                 <li>FQAs</li>
             </ul>
             <section className={`${style.nav__sectionButton}`}>
-                <button>Crear Cuenta</button>
-                <button>Ingresar</button>
+                <button className="bg-botonNaranja">Crear Cuenta</button>
+                <button className="bg-botonVerde">Ingresar</button>
                 <div className="">
                     <MdSearch />
                 </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,6 +12,10 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        'botonNaranja' : '#FF7D27',
+        'botonVerde' : '#8BC34A'
+      }
     },
   },
   plugins: [],
